76e7ac426af7c9e43f359b8fc80d39fbc9d40cd1
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-11-15 16:44:12 +0000

    Confirm ruby install

--- Artifacts/linux-ruby/linux-ruby.sh
+++ Artifacts/linux-ruby/linux-ruby.sh
@@ -4,4 +4,4 @@ rubyversion=$1
 
 sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update
-sudo apt-get install "ruby${rubyversion}" "ruby${rubyversion}-dev"
+sudo apt-get install -y "ruby${rubyversion}" "ruby${rubyversion}-dev"
